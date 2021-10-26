27bbf62b328187b7ff9de641fdba4b3bf9ad212d
Ivan Boyd <iboyd3@cnm.edu>
2021-10-26 11:27:58 -0600

    11:30

--- Light1DiffusePIX-2pin/Light1DiffusePIX-2pin.ino
+++ Light1DiffusePIX-2pin/Light1DiffusePIX-2pin.ino
@@ -9,22 +9,24 @@
       03 - add random color, lisght strip as rainbow, send maize & blue down strip
    Author: Ivan Boyd
    Date: 10/25/2021, 11:50AM (begin)
+   legend: IB = Infinity Box, NP = NeoPixel
  */
-//#include <U>
 #include <Adafruit_NeoPixel.h>
 const int PIXELPIN_1 = 15, PIXELPIN_2 = 16,   PIXELCOUNT = 1; 
     int n = 0,           // will use as pix counter in array
         red_rgb[]   = {245, 53, 10},
         green_rgb[] = {51, 190, 52},
         blue_rgb[]  = {74, 76, 136},
+        off_hex     = 0x000000,
         
 // set rgb to color hex
       color_hex[] = {0xdf2523,0x1c8d3a,0x282aa9},  // where 1st param R, 2nd G, 3rd B
       rgb[]       = {0,1,2},
       red=0, green=1, blue=2,
       off_rgb[]   = {0,0,0},
-      bri   = 0,
-      del=50   //delay
+      bri[]       = {25,100,255},
+      low = 3, med = 75, high = 255,
+      del=50,   ckDel = 2000      //delay
       ;
 Adafruit_NeoPixel pixel_1(PIXELCOUNT, PIXELPIN_1, NEO_RGB + NEO_KHZ800); // declare object
 Adafruit_NeoPixel pixel_2(PIXELCOUNT, PIXELPIN_2, NEO_GRB + NEO_KHZ800); // declare object
@@ -38,25 +40,55 @@ Adafruit_NeoPixel pixel_2(PIXELCOUNT, PIXELPIN_2, NEO_GRB + NEO_KHZ800); // decl
  */ 
 
 void setup() {
-   Serial.begin(9600);    //set up a monitor
+  Serial.begin(9600);    //set up a monitor
   while(!Serial);
-  // n is pix# being set
-  Serial.printf("Begin Set Up n: %i PIXELCOUNT %i\n",n,PIXELCOUNT);
+  runNPchk();
 
+}
+
+
+void loop() {
+}
+
+void runNPchk() {       // Turns NP's OFF, then ON Low for ckDel(3 Sec's), ditto Med & High
+    NPsOff();
+    delay(1000);
+    NPsOn(low);
+    delay(ckDel);
+    NPsOff();
+    NPsOn(med);
+    delay(ckDel);
+    NPsOff(); 
+    NPsOn(high);
+    delay(ckDel);
+    NPsOff();
+}
+
+void NPsOn(int _bri) {  // Turns All NP's On brightness level set a parameter
+ // n is pix# being set
+  Serial.printf("Turn NP's On n: %i PIXELCOUNT %i\n",n,PIXELCOUNT);
   pixel_1.begin();
   pixel_2.begin();
-  bri = 100;
-  pixel_1.setBrightness(bri); 
-  pixel_2.setBrightness(bri);  
- 
-
-  pixel_1.setPixelColor(n, 0xFF0000);   // Sets to my Red choice r, g, b = 0-255 
-  pixel_2.setPixelColor(1, 0xFF0000);   // Sets to my Red choice r, g, b = 0-255 
-  delay(500);
+//  bri = 100;
+  pixel_1.setBrightness(_bri); 
+  pixel_2.setBrightness(_bri);  
+  pixel_1.setPixelColor(0, 0xFF0000);   // Sets to my Red choice r, g, b = 0-255 
+  pixel_2.setPixelColor(0, 0xFF0000);   // Sets to my Red choice r, g, b = 0-255 
+//  delay(500);
   pixel_1.show(); 
   pixel_2.show(); 
 }
 
+void NPsOff() {         // Turns All NP's Off ()x000000
+  Serial.printf("turn NP's off, n: %i PIXELCOUNT %i\n",n,PIXELCOUNT);
+  pixel_1.setPixelColor(0, 0x000000);   //  Turn off pixels
+  pixel_2.setPixelColor(0, 0x000000);   // Turn off pixels
+  pixel_1.show(); 
+  pixel_2.show(); 
+}
+
+
+//  *** OLD STUFF THAT MIGHT BE USEFULL
 // pixel.setPixelColor(n, 0x0000FF);   // Sets to my Red choice r, g, b = 0-255 
 //    pixel.setPixelColor(1, 0x0000FF);   // Sets to my Red choice r, g, b = 0-255 
 //
@@ -66,11 +98,3 @@ void setup() {
 //  pixel.setPixelColor(n, color_hex[green]);   // Sets to my Red choice r, g, b = 0-255 
 //    pixel.setPixelColor(1, color_hex[green]);   // Sets to my Red choice r, g, b = 0-255 
 //  pixel.setBrightness(bri);  
-
-
-
-void loop() {
-  
-
-
-}
---
+++ sensor2light/sensor2light.ino
@@ -0,0 +1,149 @@
+/* program: sensor2light
+ * Ivan Boyd    10/26/2021
+ *  Intent of program is to combine dist-sensor and NeoPix programs and
+ *  tie distance to led value intensity, ie, when object is further away
+ *  the LEDs are dimmer
+ *  
+ *  History:
+ * - Light1DiffusePIX-2pin ... sets up Adafruit libs to call both RGB & GRB 
+ *    NEOPIXELS
+ * - HC-SR04-DistSensor-function   ... sets up distance sensor and prints in cm
+ *  From sample code on using the HC-SR04
+ *  
+ *  Legend: "IB" = Infinity Box   "obj" = object    "NP" = NEOPIXEL
+*/
+//  Set up Adafruit Environment
+#include <Adafruit_NeoPixel.h>
+const int PIXELPIN_1 = 15, PIXELPIN_2 = 16,   PIXELCOUNT = 1; 
+    int n = 0,           // will use as pix counter in array
+        red_rgb[]   = {245, 53, 10},
+        green_rgb[] = {51, 190, 52},
+        blue_rgb[]  = {74, 76, 136},
+        off_hex     = 0x000000,
+        
+// set rgb to color hex
+      color_hex[] = {0xdf2523,0x1c8d3a,0x282aa9},  // where 1st param R, 2nd G, 3rd B
+      rgb[]       = {0,1,2},
+      red=0, green=1, blue=2,
+      off_rgb[]   = {0,0,0},
+      bri[]       = {25,100,255},
+      low = 3, med = 75, high = 255,          // for NP light intensity
+      quickDel=50,   ckDel = 1000, longDel =  3000      //delay
+      ;
+Adafruit_NeoPixel pixel_1(PIXELCOUNT, PIXELPIN_1, NEO_RGB + NEO_KHZ800); // declare object
+Adafruit_NeoPixel pixel_2(PIXELCOUNT, PIXELPIN_2, NEO_GRB + NEO_KHZ800); // declare object
+
+
+// Trig & Echo pins
+const int trigPin = 12,   echoPin = 11,   maxDist = 1198, minDist = 1;
+bool  inRange;            // use to test if something is in the range
+                          // of the sensor
+
+int duration = 0, distance = 0;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+  while(!Serial);
+  Serial.printf("Printer initialized\n");
+  delay(500);
+  runNPchk();   // Turns NP's OFF, then ON Low for ckDel(3 Sec's), 
+                //ditto Med & High
+  NPsOff();
+  inRange = !inRange;
+  
+}
+
+void loop() {
+ 
+  distance = getDist(trigPin, duration, distance);
+  inRange = distance < maxDist && distance > minDist;
+  Serial.printf("inRange: %i, Distance = %icm \n", inRange, distance);   // Print dist to Serial Mon
+  delay(500);
+  if (inRange) {
+    Serial.printf("Object has entered scanning field: %i, Distance = %icm \n", inRange, distance);   // Print dist to Serial Mon
+    showNPlvl(distance);
+
+  }
+  else {
+    NPsOff();
+  }
+
+//    new_Pix_P = map(position, enc_Low, enc_High, pix_Low, pix_High);
+
+//      Serial.printf("mapping Neo pi's: %i %i \n",position, new_Pix_P);
+  
+}           // *** END MAIN VOID LOOP ***
+
+
+                  //  FUNCTIONS //
+
+
+void showNPlvl(int _distance) {
+    if (_distance > 700) {
+      NPsOn(low);
+    }
+    if (_distance > 200 && _distance < 700) {
+      NPsOn(med);
+    }
+    if (_distance < 200) {
+      NPsOn(high);
+    }
+}
+
+void runNPchk() {       // Turns NP's OFF, then ON Low for ckDel(3 Sec's), ditto Med & High
+    NPsOff();
+    delay(1000);
+    NPsOn(low);
+    delay(ckDel);
+    NPsOff();
+    NPsOn(med);
+    delay(ckDel);
+    NPsOff(); 
+    NPsOn(high);
+    delay(ckDel);
+    NPsOff();
+}
+
+void NPsOn(int _bri) {  // Turns All NP's On brightness level set a parameter
+ // n is pix# being set
+  Serial.printf("Turn NP's On n: %i PIXELCOUNT %i\n",n,PIXELCOUNT);
+  pixel_1.begin();
+  pixel_2.begin();
+//  bri = 100;
+  pixel_1.setBrightness(_bri); 
+  pixel_2.setBrightness(_bri);  
+  pixel_1.setPixelColor(0, 0xFF0000);   // Sets to my Red choice r, g, b = 0-255 
+  pixel_2.setPixelColor(0, 0xFF0000);   // Sets to my Red choice r, g, b = 0-255 
+//  delay(500);
+  pixel_1.show(); 
+  pixel_2.show(); 
+}
+
+void NPsOff() {         // Turns All NP's Off ()x000000
+  Serial.printf("turn NP's off, n: %i PIXELCOUNT %i\n",n,PIXELCOUNT);
+  pixel_1.setPixelColor(0, 0x000000);   //  Turn off pixels
+  pixel_2.setPixelColor(0, 0x000000);   // Turn off pixels
+  pixel_1.show(); 
+  pixel_2.show(); 
+}
+
+                  
+int getDist(int _trigPin,int _duration,int _distance) {
+ // Clear trigPin/Set LOW
+  digitalWrite(_trigPin, LOW);
+  delayMicroseconds(5);
+
+  //trigger sensor by setting tripPin high for 10 microseconds
+  digitalWrite(_trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(_trigPin, LOW);
+
+  // Read the echoPin, pulseIn() returns the dur length of pulse
+  //  in microseconds.
+  _duration = pulseIn(echoPin, HIGH);
+  // Calculate the dist:
+  _distance = _duration *0.034 / 2;
+  return _distance;
+}
