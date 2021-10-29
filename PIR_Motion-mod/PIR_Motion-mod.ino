f7aa7b2bd0f016d8189e313d4c48ffcf3bdca6d2
Ivan Boyd <iboyd3@cnm.edu>
2021-10-29 17:40:37 -0600

    final stretch

---
+++ PIR_Motion-mod/PIR_Motion-mod.ino
@@ -0,0 +1,55 @@
+
+//======================================
+int timeSeconds = 10;
+
+
+// Set GPIOs for LED and PIR Motion Sensor
+const int led = 16;
+const int motionSensor = 17;
+
+// Timer: Auxiliary variables
+unsigned long now = millis();
+unsigned long lastTrigger = 0;
+boolean startTimer = false;
+
+// Checks if motion was detected, sets LED HIGH and starts a timer
+
+void setup() {
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+    runPRTchk();        // Start print to monitor
+
+  
+  // PIR Motion Sensor mode INPUT_PULLUP
+  pinMode(motionSensor, INPUT_PULLUP);
+  // Set motionSensor pin as interrupt, assign interrupt function and set RISING mode
+//  attachInterrupt(digitalPinToInterrupt(motionSensor), detectsMovement, RISING);
+
+  // Set LED to LOW
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);
+}
+
+void loop() {
+  // Current time
+  now = millis();
+  // Turn off the LED after the number of seconds defined in the timeSeconds variable
+  if(startTimer && (now - lastTrigger > (timeSeconds*1000))) {
+    Serial.println("Motion stopped...");
+    digitalWrite(led, LOW);
+    startTimer = false;
+  }
+}
+
+void runPRTchk() {               // Start print to monitor
+  Serial.begin(9600);
+  while(!Serial);
+  Serial.printf("Printer initialized\n");
+  delay(1000);
+}
+//void IRAM_ATTR detectsMovement() {
+////  Serial.println("MOTION DETECTED!!!");
+//  digitalWrite(led, HIGH);
+//  startTimer = true;
+//  lastTrigger = millis();
+//}
